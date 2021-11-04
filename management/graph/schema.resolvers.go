7ea5fbb446d107d9ab33decafcb6b73470b64f76
bobby ross <bobby.dev@pm.me>
2021-11-04 11:25:48 +0000

    feat: add create organisation ddb implementation to resolver

--- management/graph/schema.resolvers.go
+++ management/graph/schema.resolvers.go
@@ -9,17 +9,31 @@ import (
 	"management/graph/generated"
 	"management/graph/model"
 	"management/internal/users"
+	"management/internal/organisations"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func (r *mutationResolver) CreateOrganisation(ctx context.Context, input *model.NewOrganisation) (*model.Organisation, error) {
-	panic(fmt.Errorf("not implemented"))
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	ddb := dynamodb.New(sess)
+
+	org, err := organisations.CreateOrganisation(ctx, ddb, "test", *input)
+
+	if err != nil {
+		fmt.Println("Could not create new organisation")
+	}
+
+	return org, nil
 }
 
 func (r *queryResolver) CurrentUser(ctx context.Context) (*model.User, error) {
-	panic(fmt.Errorf("not implemented"))
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
