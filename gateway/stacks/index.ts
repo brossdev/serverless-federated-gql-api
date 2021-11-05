3ec3fb072d4e64708edba7453d0aaa8130141097
bobby ross <bobby.dev@pm.me>
2021-11-05 13:47:57 +0000

    build: upgrade to node 14 default runtime

--- gateway/stacks/index.ts
+++ gateway/stacks/index.ts
@@ -5,11 +5,10 @@ import GatewayStack from "./Gateway";
 export default function main(app: sst.App): void {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-      runtime: "nodejs12.x",
+      runtime: "nodejs14.x",
   });
     
   new DataStoreStack(app, "data-store")
   new GatewayStack(app, "gateway");
 
-  // Add more stacks
 }
