8f3a835cc168951e461806120b27b5e1ffd7645d
bobby ross <bobby.dev@pm.me>
2021-11-15 11:22:27 +0000

    feat: add initial login page setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Auth from "@aws-amplify/auth";
+import { AuthContext } from "./contexts/auth-context";
+import "./App.css";
 
-const AuthenticatedApp = React.lazy(/* webpackPrefetch: true */ () => import('./authenticated-app'));
-const UnAuthenticatedApp = React.lazy(() => import('./unauthenticated-app'));
+const AuthenticatedApp = React.lazy(
+  /* webpackPrefetch: true */ () => import("./authenticated-app")
+);
+const UnAuthenticatedApp = React.lazy(() => import("./unauthenticated-app"));
 
 function App() {
-    const user = false;
-  return (
+  const [isAuthenticating, setIsAuthenticating] = React.useState(true);
+  const [user, setUser] = React.useState(null);
+
+  async function onLoad() {
+    try {
+      const authUser = await Auth.currentAuthenticatedUser();
+      setUser(authUser);
+    } catch (error) {
+      if (error !== "No current User") {
+        console.log({ error });
+      }
+    } finally {
+      setIsAuthenticating(false);
+    }
+  }
+
+  React.useEffect(() => {
+    onLoad();
+  }, []);
+
+  return isAuthenticating ? (
+    <div>Loading ...</div>
+  ) : (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <React.Suspense fallback={<div>Loading...</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
         <main>
-            {user ? <AuthenticatedApp/> : <UnAuthenticatedApp />}
+          {user ? (
+            <AuthContext.Provider value={user}>
+              <AuthenticatedApp />
+            </AuthContext.Provider>
+          ) : (
+            <UnAuthenticatedApp />
+          )}
         </main>
-            </React.Suspense>
+      </React.Suspense>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Amplify } from "@aws-amplify/core";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+console.log({ env: process.env });
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: process.env.REACT_APP_AWS_REGION,
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
