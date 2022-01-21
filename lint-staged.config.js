74739d22c256d2a4216cb55ddfc21ba63356d14a
Bobby Ross <bobby.dev@pm.me>
2022-01-21 11:50:49 +0000

    build: update lint staged to use yarn for consistancy

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx)': [`npm run format`],
-  '*.+(js|jsx|ts|tsx)': [`npm run lint`],
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx)': [`yarn run format`],
+  '*.+(js|jsx|ts|tsx)': [`yarn run lint`],
   '*.go': ['gofmt -w'],
 };
