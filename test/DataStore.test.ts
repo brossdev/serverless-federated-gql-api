41501985261fe218bdc7350c811487fdc687e22b
Bobby Ross <bobby.dev@pm.me>
2022-02-20 12:10:23 +0000

    build: update sst version to 0.65 and cdk libs to new cdk v2 format, update test to new assertion config

--- test/DataStore.test.ts
+++ test/DataStore.test.ts
@@ -1,4 +1,4 @@
-import { expect, haveResource } from '@aws-cdk/assert';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as sst from '@serverless-stack/resources';
 import DataStore from '../stacks/DataStore';
 
@@ -6,6 +6,9 @@ test('Test Data Store Stack', () => {
   const app = new sst.App();
   // WHEN
   const stack = new DataStore(app, 'test-datastore');
+
+  const template = Template.fromStack(stack);
   // THEN
-  expect(stack).to(haveResource('AWS::DynamoDB::Table'));
+
+  template.resourceCountIs('AWS::DynamoDB::Table', 1);
 });
